d0e4d1b08a433ea64298d18b29429900a157c74e
Bhavya <121705262+bhavya681@users.noreply.github.com>
2024-04-10 15:46:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 // vite.config.js
 import { defineConfig } from 'vite';
-import externalImport from 'vite-plugin-external-import';
 
 export default defineConfig({
-  plugins: [
-    externalImport({
-      // Exclude mongoose from being processed by Vite
-      exclude: ['mongoose'],
-    }),
-  ],
+  build: {
+    rollupOptions: {
+      external: ['mongoose'], // Exclude mongoose from being bundled
+    },
+  },
 });
